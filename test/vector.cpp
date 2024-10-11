cb359e0c9ad8790b35a5de6033adf7298efcd881
jeff <jeffyan167@gmail.com>
2024-10-11 12:51:57 +0800

    添加了vector容器类的测试文件

--- test/vector.cpp
+++ test/vector.cpp
@@ -1,8 +1,11 @@
 #include "../src/vector.hpp"
-
+#include <cassert>
+#include <iostream>
 
 int main() {
   mystl::vector<int> v;
-  std::assert(v.size() == 0);
-  
+  assert(v.size() == 0);
+  assert(v.capacity() == 0);
+  assert(v.empty());
+  std::cout << "Hello" << std::endl;
 }
