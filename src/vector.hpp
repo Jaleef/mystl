5bf695c4b4c04fcda93c8e538034a6c052e12f0c
jeff <jeffyan167@gmail.com>
2024-10-11 13:19:09 +0800

    完成了大小构造函数, 析构函数和重载运算符

--- src/vector.hpp
+++ src/vector.hpp
@@ -7,7 +7,18 @@ class vector {
   public:
     typedef T* iterator;
 
+    // 空构造函数
     vector() : _start(nullptr), _finish(nullptr), _endOfStorage(nullptr) {}
+
+    // 根据大小构造函数
+    vector(size_t n, const T& value = T()) {
+      _start = new T[n];
+      _finish = _start + n;
+      _endOfStorage = _finish;
+      for (size_t i = 0 ; i < n ; ++i) {
+        _start[i] = value;
+      }
+    }
 
     std::size_t size() const { 
       return _finish - _start; 
@@ -21,6 +32,18 @@ class vector {
       return _start == _finish;
     }
 
+    // 重载[]运算符
+    T& operator[](size_t pos) {
+      return _start[pos];
+    }
+
+    // 析构函数
+    ~vector() {
+      if (_start) {
+        delete[] _start;
+        _start = _finish = _endOfStorage = nullptr;
+      }
+    }
 
   private:
     iterator _start;
