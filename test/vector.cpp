4ab68bc414409145e49f7af919cfd4439216dd30
jeff <jeffyan167@gmail.com>
2024-10-11 14:11:06 +0800

    完成了vector测试

--- test/vector.cpp
+++ test/vector.cpp
@@ -7,5 +7,30 @@ int main() {
   assert(v.size() == 0);
   assert(v.capacity() == 0);
   assert(v.empty());
-  std::cout << "Hello" << std::endl;
+  std::cout << "TestEmptyVector passed" << std::endl;
+
+  mystl::vector<int> v2(5, 10);
+  assert(v2.size() == 5);
+  assert(v2.capacity() == 5);
+  assert(!v2.empty());
+  for (size_t i = 0 ; i < 5 ; ++i) {
+    assert(v2[i] == 10);
+  }
+  std::cout << "TestInitVector passed" << std::endl;
+
+  mystl::vector<int> v3(v2);
+  assert(v3.size() == v2.size());
+  assert(v3.capacity() == v3.capacity());
+  for (size_t i = 0 ; i < v3.size() ; ++i) {
+    assert(v3[i] == 10);
+  }
+  std::cout << "TestCopyVector passed" << std::endl;
+
+  mystl::vector<int> v4 = v2;
+  assert(v4.size() == v2.size());
+  assert(v4.capacity() == v2.capacity());
+  for (size_t i = 0 ; i < v4.size() ; ++i) {
+    assert(v4[i] == 10);
+  }
+  std::cout << "TestAssignVector passed" << std::endl;
 }
